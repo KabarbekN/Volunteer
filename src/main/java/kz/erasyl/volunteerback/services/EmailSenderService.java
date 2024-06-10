1c48cba75d9a93d722f5e1ffa80ec6c6e388f268
KabarbekN <nkabarbek03@gmail.com>
2024-06-11 02:25:02 +0500

    a lot of new func

--- src/main/java/kz/erasyl/volunteerback/services/EmailSenderService.java
+++ src/main/java/kz/erasyl/volunteerback/services/EmailSenderService.java
@@ -1,5 +1,6 @@
 package kz.erasyl.volunteerback.services;
 
+import kz.erasyl.volunteerback.requests.ContactUsRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,10 +8,15 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Service
 @RequiredArgsConstructor
 public class EmailSenderService {
     private final JavaMailSender mailSender;
+    private final ExecutorService executorService = Executors.newCachedThreadPool();
+
     @Value("${spring.mail.username}")
     private String username;
 
@@ -22,6 +28,20 @@ public class EmailSenderService {
         message.setText(text);
         mailSender.send(message);
         System.out.println("Message sended");
+    }
+
+    public Boolean contactUsEmail(ContactUsRequest request){
+        executorService.submit(() -> {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(request.getEmail());
+            message.setTo(username);
+            message.setSubject(request.getName());
+            message.setText(request.getMessage());
+            mailSender.send(message);
+            System.out.println("Message sended");
+            return true;
+        });
+        return true;
     }
 
 }
