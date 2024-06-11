703a32d273d67049f727cf8fbf06c874855ed220
KabarbekN <nkabarbek03@gmail.com>
2024-06-11 14:25:37 +0500

    a lot of new func

--- src/main/java/kz/erasyl/volunteerback/models/enums/EventType.java
+++ src/main/java/kz/erasyl/volunteerback/models/enums/EventType.java
@@ -3,4 +3,12 @@ package kz.erasyl.volunteerback.models.enums;
 public enum EventType {
     SOCIAL,
     ECO,
+    MEDICAL,
+    ONLINE,
+
+    ANIMAL_CARE,
+            CULTURAL,
+    SPORTS,
+    ELDERLY_CARE,
+    FUNDING
 }
